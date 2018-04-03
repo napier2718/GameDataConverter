3ca3c7d34200b3ce82ec8d66b4d3eb2355d0f996
napier2718 <37551412+napier2718@users.noreply.github.com>
2018-04-03 13:34:09 +0900

    一部関数化

--- GameDataConverter/Main.cpp
+++ GameDataConverter/Main.cpp
@@ -10,15 +10,6 @@
 #define TEXTSIZE 128
 
 using namespace std;
-using namespace rapidjson;
-
-
-void EncodeUtf8ToShiftjis(char *shiftjis, const char *utf8)
-{
-  static wchar_t wbuf[TEXTSIZE];
-  MultiByteToWideChar(CP_UTF8, 0, utf8, -1, wbuf, TEXTSIZE);
-  WideCharToMultiByte(CP_ACP, 0, wbuf, -1, (LPSTR)shiftjis, TEXTSIZE, " ", NULL);
-}
 
 enum ObjectType
 {
@@ -51,24 +42,25 @@ namespace std
     }
   };
 }
-
-int main()
+void EncodeUtf8ToShiftjis(char *shiftjis, const char *utf8)
 {
-  vector<Object> bgList, objectList, linkList;
-  unordered_map<string, int> imageMap;
-  vector<string> imageMapList;
-  unordered_map<pair<string, int>, int> fontMap;
-  vector<pair<string, int>> fontMapList;
+  static wchar_t wbuf[TEXTSIZE];
+  MultiByteToWideChar(CP_UTF8, 0, utf8, -1, wbuf, TEXTSIZE);
+  WideCharToMultiByte(CP_ACP, 0, wbuf, -1, (LPSTR)shiftjis, TEXTSIZE, " ", NULL);
+}
+void ReadJSONFile(const char *jsonFileName, vector<string> &imageList, vector<pair<string, int>> &fontList, vector<Object> &bgList, vector<Object> &objectList, vector<Object> &linkList)
+{
+  using namespace rapidjson;
   FILE *jsonFile;
-  fopen_s(&jsonFile, "data\\title.json", "r");
-  char cBuffer[65536];
-  char cTemp[TEXTSIZE];
-  FileReadStream jsonFS(jsonFile, cBuffer, sizeof(cBuffer));
+  char cJSONBuffer[65536], cTexiBuffer[TEXTSIZE];
   Document jsonData;
+  unordered_map<string, int> imageMap;
+  unordered_map<pair<string, int>, int> fontMap;
+  fopen_s(&jsonFile, jsonFileName, "r");
+  FileReadStream jsonFS(jsonFile, cJSONBuffer, sizeof(cJSONBuffer));
   jsonData.ParseStream(jsonFS);
   const Value &layers = jsonData["layers"];
-  const int layerNum = layers.Size();
-  for (int i = 0; i < layerNum; i++) {
+  for (unsigned int i = 0; i < layers.Size(); i++) {
     const Value &objects = layers[i]["objects"];
     const int objectNum = objects.Size();
     for (int j = 0; j < objectNum; j++) {
@@ -83,36 +75,36 @@ int main()
         if (strncmp(objects[j]["properties"]["link"].GetString(), "start", 5) == 0) object.link = start;
         else if (strncmp(objects[j]["properties"]["link"].GetString(), "end", 3) == 0) object.link = LinkType::end;
       }
-      object.posX  = objects[j]["x"].GetInt();
-      object.posY  = objects[j]["y"].GetInt();
+      object.posX = objects[j]["x"].GetInt();
+      object.posY = objects[j]["y"].GetInt();
       object.sizeX = objects[j]["width"].GetInt();
       object.sizeY = objects[j]["height"].GetInt();
       switch (object.type) {
       case image:
-        {
-          string image(objects[j]["properties"]["filePath"].GetString());
-          if (imageMap.find(image) == imageMap.end()) {
-            object.id = (int)(imageMapList.size());
-            imageMap[image] = object.id;
-            imageMapList.push_back(image);
-          }
-          else object.id = imageMap[image];
+      {
+        string image(objects[j]["properties"]["filePath"].GetString());
+        if (imageMap.find(image) == imageMap.end()) {
+          object.id = (int)(imageList.size());
+          imageMap[image] = object.id;
+          imageList.push_back(image);
         }
-        break;
+        else object.id = imageMap[image];
+      }
+      break;
       case text:
-        {
-          EncodeUtf8ToShiftjis(cTemp, objects[j]["text"]["fontfamily"].GetString());
-          pair<string, int> font = make_pair(string(cTemp), objects[j]["text"]["pixelsize"].GetInt());
-          if (fontMap.find(font) == fontMap.end()) {
-            object.id = (int)(fontMapList.size());
-            fontMap[font] = object.id;
-            fontMapList.push_back(font);
-          }
-          else object.id = fontMap[font];
+      {
+        EncodeUtf8ToShiftjis(cTexiBuffer, objects[j]["text"]["fontfamily"].GetString());
+        pair<string, int> font = make_pair(string(cTexiBuffer), objects[j]["text"]["pixelsize"].GetInt());
+        if (fontMap.find(font) == fontMap.end()) {
+          object.id = (int)(fontList.size());
+          fontMap[font] = object.id;
+          fontList.push_back(font);
         }
-        EncodeUtf8ToShiftjis(cTemp, objects[j]["text"]["text"].GetString());
-        object.text += cTemp;
-        break;
+        else object.id = fontMap[font];
+      }
+      EncodeUtf8ToShiftjis(cTexiBuffer, objects[j]["text"]["text"].GetString());
+      object.text += cTexiBuffer;
+      break;
       }
       if (object.type == link) linkList.push_back(object);
       else {
@@ -122,23 +114,30 @@ int main()
     }
   }
   fclose(jsonFile);
+}
+void WriteString(string &text, FILE *&file)
+{
+  int length = (int)(text.size());
+  fwrite(&length, sizeof(int), 1, file);
+  fwrite(text.c_str(), sizeof(char), length, file);
+}
+int main()
+{
+  vector<string> imageList;
+  vector<pair<string, int>> fontList;
+  vector<Object> bgList, objectList, linkList;
+  ReadJSONFile("data\\title.json", imageList, fontList, bgList, objectList, linkList);
 
   FILE *dataFile;
   fopen_s(&dataFile, "data\\title.data", "wb");
-  size_t size = imageMapList.size();
+  size_t size = imageList.size();
+  fwrite(&size, sizeof(int), 1, dataFile);
+  for (size_t i = 0; i < size; i++) WriteString(imageList[i], dataFile);
+  size = fontList.size();
   fwrite(&size, sizeof(int), 1, dataFile);
   for (size_t i = 0; i < size; i++) {
-    int length = (int)(imageMapList[i].size());
-    fwrite(&length, sizeof(int), 1, dataFile);
-    fwrite(imageMapList[i].c_str(), sizeof(char), length, dataFile);
-  }
-  size = fontMapList.size();
-  fwrite(&size, sizeof(int), 1, dataFile);
-  for (size_t i = 0; i < size; i++) {
-    int length = (int)(fontMapList[i].first.size());
-    fwrite(&length, sizeof(int), 1, dataFile);
-    fwrite(fontMapList[i].first.c_str(), sizeof(char), length, dataFile);
-    fwrite(&fontMapList[i].second, sizeof(int), 1, dataFile);
+    WriteString(fontList[i].first, dataFile);
+    fwrite(&fontList[i].second, sizeof(int), 1, dataFile);
   }
   size = objectList.size() + bgList.size();
   fwrite(&size, sizeof(int), 1, dataFile);
@@ -148,22 +147,14 @@ int main()
     fwrite(&bgList[i].type, sizeof(ObjectType), 1, dataFile);
     fwrite(&bgList[i].posX, sizeof(int), 4, dataFile);
     fwrite(&bgList[i].id, sizeof(int), 1, dataFile);
-    if (bgList[i].type == text) {
-      int length = (int)(bgList[i].text.size());
-      fwrite(&length, sizeof(int), 1, dataFile);
-      fwrite(bgList[i].text.c_str(), sizeof(char), length, dataFile);
-    }
+    if (bgList[i].type == text) WriteString(bgList[i].text, dataFile);
   }
   size = objectList.size();
   for (size_t i = 0; i < size; i++) {
     fwrite(&objectList[i].type, sizeof(ObjectType), 1, dataFile);
     fwrite(&objectList[i].posX, sizeof(int), 4, dataFile);
     fwrite(&objectList[i].id, sizeof(int), 1, dataFile);
-    if (objectList[i].type == text) {
-      int length = (int)(objectList[i].text.size());
-      fwrite(&length, sizeof(int), 1, dataFile);
-      fwrite(objectList[i].text.c_str(), sizeof(char), length, dataFile);
-    }
+    if (objectList[i].type == text) WriteString(objectList[i].text, dataFile);
   }
   size = linkList.size();
   fwrite(&size, sizeof(int), 1, dataFile);
